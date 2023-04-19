fb7ff17ac24a83f1c3dd24f05e74516539bb770c
skylarbsandler <skylarbsandler@gmail.com>
2023-04-19 14:13:55 -0600

    github

---
+++ LaunchElectionDay/Election.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaunchElectionDay
+{
+    public class Election
+    {
+        public string Year;
+        public List<Race> Races = new List<Race>();
+
+        public Election(string year)
+        {
+            Year = year;
+            List<Race> Races = new List<Race>();
+        }
+
+        public string GetYear()
+        {
+            return $"{Year}";
+        }
+
+        public List<Race> GetRaces()
+        {
+
+        }
+    }
+}
