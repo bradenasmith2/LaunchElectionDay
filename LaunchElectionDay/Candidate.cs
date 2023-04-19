44bab54c3fadf194737bb15d75423c3924fa4c32
bradenasmith2 <bradenasmith@icloud.com>
2023-04-19 14:22:15 -0500

    git

--- LaunchElectionDay/Candidate.cs
+++ LaunchElectionDay/Candidate.cs
@@ -1,4 +1,23 @@
 ﻿namespace LaunchElectionDay
 {
-    public class Candidate { }
+    public class Candidate
+    {
+        public string Name;
+        public string Party;
+        public int Votes;
+
+        public Candidate(string name, string party)
+        {
+            Name = name;
+            Party = party;
+
+            int votes = Votes;
+        }
+
+        public void VoteFor()
+        {
+            Votes++;
+        }
+    }
+
 }
