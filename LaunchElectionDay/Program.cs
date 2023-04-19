f24474182784f1b1f8962dab071b03b46bab9871
skylarbsandler <skylarbsandler@gmail.com>
2023-04-19 13:57:27 -0600

    github

--- LaunchElectionDay/Program.cs
+++ LaunchElectionDay/Program.cs
@@ -12,3 +12,17 @@ diana.VoteFor();
 diana.VoteFor();
 
 Console.WriteLine(diana.Votes);
+
+var race = new Race("City Council District 10");
+
+
+Console.WriteLine(race.Office);
+
+Console.WriteLine(race.Candidates);
+
+race.RegisterCandidate(diana);
+
+foreach(var candidate in race.Candidates)
+{ 
+    Console.WriteLine(candidate.Name);
+}
--- LaunchElectionDay/Race.cs
+++ LaunchElectionDay/Race.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace LaunchElectionDay
 {
-    internal class Race
+    public class Race
     {
+        public string Office;
+        public List<Candidate> Candidates { get; set; } = new List<Candidate>();
+
+        public Race(string office)
+        {
+            Office = office;
+            List<Candidate> Candidates = new List<Candidate>();
+        }
+
+        public void RegisterCandidate(Candidate candidate)
+        {
+            Candidates.Add(candidate);
+        }
     }
 }
